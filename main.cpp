503942764720a957b2f05a0f721ecfeca7ab4acd
Leo Khachatorians <leokhachatorians@gmail.com>
2017-02-19 00:24:44 -0800

    Fleshing out functions

--- main.cpp
+++ main.cpp
@@ -6,19 +6,49 @@ const int MAXROWS = 20;
 const int MAXCOLS = 20;
 const int MAXSHOTLEN = 5;
 
-enum {
+enum Direction{
     UP,
     DOWN,
     LEFT,
     RIGHT
 };
 
-
-
 class Player {
+    Player(int row, int col);
+    ~Player();
+
+    public:
+        bool is_alive() const;
+        int get_row() const;
+        int get_col() const;
+        int get_kills() const;
+        int get_health() const;
+
+        void move(Direction);
+        void add_kill(int kill);
+        void reduce_health();
+
+    private:
+        int m_health;
+        int m_kills;
+        bool alive;
 };
 
 class Tank {
+    Tank(int row, int col);
+    ~Tank();
+    
+    public:
+        int get_row() const;
+        int get_col() const;;
+        int get_health() const;
+
+        void move(Direction);
+
+    private:
+        int m_health;
+        bool alive;
+
 };
 
 class Arena {
