23b81c4739f26e531cc46a735a767b211511ce24
Leo Khachatorians <leokhachatorians@gmail.com>
2017-02-19 01:45:26 -0800

    Working on display + movement
    Laying out a rough sketch of how I want to display
    movement, etc. Need to incorporate an Arena class which would
    handle redraws n stuff I think

--- main.cpp
+++ main.cpp
@@ -1,10 +1,10 @@
 #include <string>
 #include <iostream>
 
-const int MAXTANKS = 100;
-const int MAXROWS = 20;
-const int MAXCOLS = 20;
-const int MAXSHOTLEN = 5;
+const int MAXTANKS = 5;
+const int MAXROWS = 30;
+const int MAXCOLS = 30;
+const int MAXSHOTLEN = 12;
 
 enum Direction{
     UP,
@@ -14,10 +14,9 @@ enum Direction{
 };
 
 class Player {
-    Player(int row, int col);
-    ~Player();
-
     public:
+        Player(int row, int col);
+        //~Player();
         bool is_alive() const;
         int get_row() const;
         int get_col() const;
@@ -25,8 +24,9 @@ class Player {
         int get_health() const;
 
         void move(Direction);
+        void shoot(Direction);
         void add_kill(int kill);
-        void reduce_health();
+        void take_damage();
 
     private:
         int m_health;
@@ -35,6 +35,14 @@ class Player {
         int m_col;
         bool m_alive;
 };
+
+Player::Player(int row, int col) {
+    m_health = 2;
+    m_kills = 0;
+    m_row = row;
+    m_col = col;
+    m_alive = true;
+}
 
 int Player::get_row() const {
     return m_row;
@@ -60,7 +68,7 @@ void Player::add_kill(int amount) {
     m_kills += amount;
 }
 
-void Player::reduce_health() {
+void Player::take_damage() {
     m_health -= 1;
     if (m_health == 0) {
         m_alive = false;
@@ -82,28 +90,43 @@ void Player::move(Direction dir) {
     }
 }
 
-
-
-
 class Tank {
-    Tank(int row, int col);
-    ~Tank();
-    
     public:
+        Tank(int row, int col);
+        ~Tank();
         int get_row() const;
-        int get_col() const;;
-        int get_health() const;
-
+        int get_col() const;
+        bool is_alive() const;
+    
+        void take_damage();
         void move(Direction);
+        void shoot(Direction);
 
     private:
         int m_health;
-        bool alive;
+        bool m_alive;
+        int m_row;
+        int m_col;
 };
 
+int Tank::get_row() const {
+    return m_row;
+}
+
+int Tank::get_col() const {
+    return m_col;
+}
+
+bool Tank::is_alive() const {
+    return m_alive;
+}
+
+
+
 class Arena {
-    Arena(int nRows, int nCols);
-    ~Arena();
+    public:
+        Arena(int nRows, int nCols);
+        ~Arena();
 
     private:
         Player *m_player;
@@ -115,4 +138,43 @@ class Arena {
 
 
 int main(int, char *[]) {
+
+    char grid[MAXROWS][MAXCOLS];
+    Player p = Player(0, 0);
+
+    for (int r = 0; r < MAXROWS; r++) {
+        for (int c = 0; c < MAXCOLS; c++) {
+            grid[r][c] = '.';
+        }
+    }
+
+    grid[p.get_row()][p.get_col()] = 'P';
+
+    for (int r = 0; r < MAXROWS; r++) {
+        for (int c = 0; c < MAXCOLS; c++) {
+            std::cout << grid[r][c];
+        }
+        std::cout << std::endl;
+    }
+
+    std::cout << std::endl;
+
+    p.move(LEFT);
+    p.move(RIGHT);
+    p.move(UP);
+
+    grid[p.get_row()][p.get_col()] = 'P';
+
+    for (int r = 0; r < MAXROWS; r++) {
+        for (int c = 0; c < MAXCOLS; c++) {
+            std::cout << grid[r][c];
+        }
+        std::cout << std::endl;
+    }
+
+    std::cout << std::endl;
+
+
+    
+
 }
