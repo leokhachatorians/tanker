3a0e38a9080cbec5ec4a64850ef1430daa2a6348
Leo Khachatorians <leokhachatorians@gmail.com>
2017-03-09 00:13:27 -0800

    implementation of simplex noise

---
+++ Simplex.cpp
@@ -0,0 +1,93 @@
+#include "Simplex.h"
+#include "Grad.h"
+
+Simplex::Simplex() {
+    F2 = 0.366025403f;  // F2 = (sqrt(3) - 1) / 2
+    G2 = 0.211324865f;  // G2 = (3 - sqrt(3)) / 6   = F2 / (1 + 2 * K)
+
+    for (int i = 0; i < 512; i++) {
+        perm[i] = p[i & 255];
+        permMod12[i] = (short)(perm[i] % 12);
+    }
+}
+
+double Simplex::dot(int g[], double x, double y) {
+    double x_value = g[0] * x;
+    double y_value = g[1] * y;
+    return x_value + y_value;
+}
+
+static float grad(int hash, float x, float y) {
+    int h = hash & 0x3F;  // Convert low 3 bits of hash code
+    float u = h < 4 ? x : y;  // into 8 simple gradient directions,
+    float v = h < 4 ? y : x;  // and compute the dot product with (x,y).
+    return ((h & 1) ? -u : u) + ((h & 2) ? -2.0f*v : 2.0f*v);
+}
+
+int Simplex::fast_floor(double x) {
+    int xi = (int) x;
+    return x < xi ? xi-1 : xi;
+}
+
+double Simplex::noise(double xin, double yin) {
+    double n0, n1, n2;
+    int i1, j1;
+
+    double s = (xin + yin) * F2;
+    int i = fast_floor(xin + s);
+    int j = fast_floor(yin + s);
+
+    double t = (i + j) * G2;
+    double X0 = i - t;
+    double Y0 = j - t;
+    double x0 = xin - X0;
+    double y0 = yin - Y0;
+    
+    if (x0 > y0) {
+        i1 = 1;
+        j1 = 0;
+    }
+    else {
+        i1 = 0;
+        j1 = 1;
+    }
+
+    double x1 = x0 - i1 + G2;
+    double y1 = y0 - j1 + G2;
+    double x2 = x0 - 1.0 + 2.0 * G2;
+    double y2 = y0 - 1.0 + 2.0 * G2;
+
+    int ii = i & 255;
+    int jj = j & 255;
+    int gi0 = permMod12[ii + perm[jj]];
+    int gi1 = permMod12[ii + i1 + perm[jj + j1]];
+    int gi2 = permMod12[ii + 1 + perm[jj + 1]];
+
+    double t0 = 0.5 - x0 * x0 - y0 * y0;
+    if (t0 < 0) {
+        n0 = 0.0;
+    }
+    else {
+        t0 *= t0;
+        n0 = t0 * t0 * dot(grad3[gi0], x0, y0);
+    }
+
+    double t1 = 0.5 - x1 * x1 - y1 * y1;
+    if (t1 < 0) {
+        n1 = 0.0;
+    }
+    else {
+        t1 *= t1;
+        n1 = t1 * t1 * dot(grad3[gi1], x1, y1);
+    }
+
+    double t2 = 0.5 - x2 * x2 - y2 * y2;
+    if (t2 < 0) {
+        n2 = 0.0;
+    }
+    else {
+        t2 *= t2;
+        n2 = t2 * t2 * dot(grad3[gi2], x2, y2);
+    }
+    return 45.23065f * (n0 + n1 + n2);
+}
