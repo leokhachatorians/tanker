85ba2ad015f4eebcb50b77fc77589d182c1612ef
Leo Khachatorians <leokhachatorians@gmail.com>
2017-03-09 00:32:02 -0800

    seems to be working

--- Screen.cpp
+++ Screen.cpp
@@ -9,21 +9,24 @@ Screen::Screen() {
     // create and populate screen
     struct winsize w;
     double noise;
+    double scale = 0.05;
     Simplex s = Simplex();
     ioctl(STDOUT_FILENO, TIOCGWINSZ, &w);
 
     for (int r = 1; r < w.ws_row - 2; r++) {
         for (int c = 2; c < w.ws_col - 1; c++) {
-            noise = s.noise(r, c);
-
-            if (noise < .2) {
-                mvaddch(r, c, '~');
+            noise = s.noise(r * scale, c * scale);
+            if (noise < .3) {
+                mvaddch(r, c, '.');
             }
-            else if (noise < .4) {
+            else if (noise > .3 && noise < .5) {
                 mvaddch(r, c, '#');
             }
+            else if (noise > .5 && noise < .75) {
+                mvaddch(r, c, 'S');
+            }
             else {
-                mvaddch(r, c, '.');
+                mvaddch(r, c, '~');
             }
         }
     }
