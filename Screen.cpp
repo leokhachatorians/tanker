4f0eb7053434137e6f015d903659c25d65741ec1
Leo Khachatorians <leokhachatorians@gmail.com>
2017-03-09 02:09:11 -0800

    messing with values

--- Screen.cpp
+++ Screen.cpp
@@ -15,23 +15,26 @@ Screen::Screen() {
     curs_set(0);
     getmaxyx(stdscr, _height, _width);
     double noise;
-    double scale = 0.015;
+    double scale = 0.01;
     Simplex s = Simplex();
     std::srand(std::time(0));
-    int ra = 1 + (std::rand() % (int)(1000));
+    int ra = 1 + (std::rand() % (int)(5000));
 
     for (int r = 1; r < _height - 2; r++) {
         for (int c = 2; c < _width; c++) {
             //noise = s.simplex_noise((r + ra) * scale, (c + ra) * scale);
             noise = s.brownian(16, r+ra, c+ra, .5, scale, 0, 255);
-            if (noise < 90) {
+            if (noise < 93) {
                 mvaddch(r, c, '~');
             }
-            else if (noise > 90 && noise < 95) {
-                mvaddch(r, c, '#');
+            else if (noise > 90 && noise < 100) {
+                mvaddch(r, c, '=');
             }
-            else if (noise > 95 && noise < 100) {
+            else if (noise > 100 && noise < 110) {
                 mvaddch(r, c, 'S');
+            }
+            else if (noise > 110 && noise < 120) {
+                mvaddch(r, c, '#');
             }
             else {
                 mvaddch(r, c, '.');
